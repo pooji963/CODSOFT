64639419b3a7e3ec480b97fb07e2006058d84fdd
pooji963 <142346440+pooji963@users.noreply.github.com>
2024-12-13 23:11:00 +0530

    Update GradeCalculator.java

--- GradeCalculator.java
+++ GradeCalculator.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
-public class Main {
+public class GradeCalculator {
 
-    // Method to calculate grade based on average percentage
+    
     public static String calculateGrade(double averagePercentage) {
         if (averagePercentage >= 90) {
             return "A";
@@ -20,28 +20,24 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Input the number of subjects
+
         System.out.print("Enter the number of subjects: ");
         int numberOfSubjects = scanner.nextInt();
 
-        // Array to store marks for each subject
+   
         int[] marks = new int[numberOfSubjects];
         int totalMarks = 0;
 
-        // Input marks for each subject
         for (int i = 0; i < numberOfSubjects; i++) {
             System.out.print("Enter marks obtained in subject " + (i + 1) + " (out of 100): ");
             marks[i] = scanner.nextInt();
             totalMarks += marks[i];
         }
 
-        // Calculate average percentage
         double averagePercentage = (double) totalMarks / numberOfSubjects;
 
-        // Determine grade
         String grade = calculateGrade(averagePercentage);
 
-        // Display results
         System.out.println("--- Results ---");
         System.out.println("Total Marks: " + totalMarks);
         System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
